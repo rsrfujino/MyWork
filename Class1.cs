f0447e82dd78e38b947684c7d5811f20aa34807c
rsrfujino <fujino@rsr.co.jp>
2022-09-15 17:41:21 +0900

    演習問題14まで

--- Class1.cs
+++ Class1.cs
@@ -143,5 +143,244 @@ namespace MyWork
             return result;
         }
 
+        /// <summary>
+        /// 演習問題8
+        /// </summary>
+        /// <returns></returns>
+        internal static string Work08()
+        {
+            var result = string.Empty;
+            var stb = new StringBuilder();
+
+            var ram = new Random();
+            var ramNum = ram.Next(1, 1000);
+
+            stb.AppendLine(String.Format("乱数：{0}", ramNum));
+
+            for (int i = 1; i <= ramNum; i++)
+            {
+                if(ramNum % i == 0)
+                {
+                    stb.AppendLine(i.ToString());
+                }
+            }
+
+            result =stb.ToString();
+            return result;
+        }
+
+        /// <summary>
+        /// 演習問題9
+        /// </summary>
+        /// <returns></returns>
+        internal static string Work09()
+        {
+            var result = string.Empty;
+            var stb = new StringBuilder();
+
+           for (int lpCnt = 1; lpCnt <= 10000; lpCnt++)
+           {
+                var targetNums = new List<int>();
+                for (int i = 1; i < lpCnt; i++)
+                {
+                    if (lpCnt % i != 0)
+                    {
+                        continue;
+                    }
+
+                    targetNums.Add(i);
+                }
+
+                if(lpCnt == targetNums.Sum())
+                {
+                    stb.AppendLine(String.Format("{0}", lpCnt));
+                }
+            }
+
+            result = stb.ToString();
+            return result;
+        }
+
+        /// <summary>
+        /// 演習問題10
+        /// </summary>
+        /// <returns></returns>
+        internal static string Work10()
+        {
+            var result = string.Empty;
+            var stb = new StringBuilder();
+
+            var ram = new Random();
+            var nums = new List<int>();
+
+            for (int lpCnt = 0; lpCnt < 10; lpCnt++)
+            {
+                nums.Add(ram.Next(1, 100));
+            }
+
+            var result1 = new List<int>();
+            var result2 = new List<int>();
+
+            nums.ForEach(x => 
+            { 
+                if(x % 2 == 0)
+                {
+                    result1.Add(x);
+                }
+                else
+                {
+                    result2.Add(x);
+                }
+            });
+
+            stb.AppendLine(string.Format("配列 {0}", string.Join(", ", nums)));
+            stb.AppendLine(string.Format("偶数 {0}", string.Join(", ", result1)));
+            stb.AppendLine(string.Format("奇数 {0}", string.Join(", ", result2)));
+
+            result = stb.ToString();
+            return result;
+        }
+
+        /// <summary>
+        /// 演習問題11
+        /// </summary>
+        /// <returns></returns>
+        internal static string Work11()
+        {
+            var result = string.Empty;
+            var stb = new StringBuilder();
+
+            var num1 = 0;
+            var num2 = 1;
+            var isLoop = true;
+            stb.AppendLine(num2.ToString());
+            while (isLoop)
+            {
+                var tmp = num1;
+                num1 = num2;
+                num2 = tmp + num2;
+                stb.AppendLine(num2.ToString());
+                if(num2 > 40)
+                {
+                    isLoop = false;
+                }
+            }
+
+            result = stb.ToString();
+            return result;
+        }
+
+        /// <summary>
+        /// 演習問題12
+        /// </summary>
+        /// <returns></returns>
+        internal static string Work12()
+        {
+            var result = string.Empty;
+            var stb = new StringBuilder();
+
+            var num1 = 0;
+            var num2 = 1;
+            var num3 = 1;
+            var isLoop = true;
+            stb.AppendLine(num3.ToString());
+            while (isLoop)
+            {
+                var tmp = num1 + num2 + num3;
+                num1 = num2;
+                num2 = num3;
+                num3 = tmp;
+                stb.AppendLine(num2.ToString());
+                if (num3 > 30)
+                {
+                    isLoop = false;
+                }
+            }
+
+            result = stb.ToString();
+            return result;
+        }
+
+        /// <summary>
+        /// 演習問題13
+        /// </summary>
+        /// <returns></returns>
+        internal static string Work13()
+        {
+            var result = string.Empty;
+            var stb = new StringBuilder();
+
+            var ram = new Random();
+            var list1 = new List<int>();
+            var list2 = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                list1.Add(ram.Next(1, 10));
+                list2.Add(ram.Next(1, 10));
+            }
+
+            var resultCom = new List<int>();
+            var resultAll = list1.Distinct().ToList();
+
+            list2.ForEach(x => 
+            {
+                if(resultAll.Exists(y=>x==y))
+                {
+                    if(!resultCom.Exists(y=>x==y))
+                    {
+                        resultCom.Add(x);
+                    }
+                }
+                else
+                {
+                    resultAll.Add(x);
+                }
+            });
+
+            stb.AppendLine(string.Format("配列1 {0}", string.Join(' ', list1)));
+            stb.AppendLine(string.Format("配列2 {0}", string.Join(' ', list2)));
+            stb.AppendLine(string.Format("共通 {0}", string.Join(' ', resultCom)));
+            stb.AppendLine(string.Format("全部 {0}", string.Join(' ', resultAll)));
+
+            result = stb.ToString();
+            return result;
+        }
+
+        /// <summary>
+        /// 演習問題14
+        /// </summary>
+        /// <returns></returns>
+        internal static string Work14()
+        {
+            var result = string.Empty;
+            var stb = new StringBuilder();
+
+            var ram = new Random();
+            var list1 = new List<int>();
+            for (int i = 2; i <= 100; i++)
+            {
+                list1.Add(i);
+            }
+
+            var isLoop = true;
+            var currentNum = 0;
+            var results = new List<int>();
+            while (isLoop)
+            {
+                currentNum = list1.Min();
+                list1.RemoveAll(x =>x % currentNum == 0);
+                results.Add(currentNum);
+
+                if(!list1.Any() || currentNum >= list1.Max())
+                {
+                    isLoop = false;
+                }
+            }
+
+            stb.AppendLine(string.Format("配列 {0}", string.Join(' ', results)));
+
+            result = stb.ToString();
+            return result;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,24 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using MyWork;
 
-Console.WriteLine("Hello, World!");
-
 //Console.WriteLine("演習問題1");
 //Console.WriteLine(Class1.Work01());
 
 //Console.WriteLine("演習問題2");
 //Console.WriteLine(Class1.Work02());
 
-Console.WriteLine("演習問題3");
-Console.WriteLine(Class1.Work03(6));
+//Console.WriteLine("演習問題3");
+//Console.WriteLine(Class1.Work03(6));
 
-Console.WriteLine("演習問題4");
-Console.WriteLine(Class1.Work04());
+//Console.WriteLine("演習問題4");
+//Console.WriteLine(Class1.Work04());
 
-Console.WriteLine("演習問題5");
-Console.WriteLine(Class1.Work05());
+//Console.WriteLine("演習問題5");
+//Console.WriteLine(Class1.Work05());
 
-Console.WriteLine("演習問題6");
-Console.WriteLine(Class1.Work05());
+//Console.WriteLine("演習問題8");
+//Console.WriteLine(Class1.Work08());
 
+//Console.WriteLine("演習問題9");
+//Console.WriteLine(Class1.Work09());
 
+//Console.WriteLine("演習問題10");
+//Console.WriteLine(Class1.Work10());
+
+Console.WriteLine("演習問題11");
+Console.WriteLine(Class1.Work11());
+
+Console.WriteLine("演習問題12");
+Console.WriteLine(Class1.Work12());
+
+Console.WriteLine("演習問題13");
+Console.WriteLine(Class1.Work13());
+
+Console.WriteLine("演習問題14");
+Console.WriteLine(Class1.Work14());
